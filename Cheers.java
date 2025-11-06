fc1f453b363c4b32bb0794a64568319fa490bf8e
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-06 12:28:21 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,24 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String cheer = args[0].toUpperCase();
+	        int times_to_cheer = Integer.parseInt(args[1]);
+                int i = 0;
+                String[] letters = cheer.split("");
+
+                // Set a or an
+                for (String letter : letters) {
+                        String a_an = "a  ";
+                        if ("AEFHILMNORSX".contains(letter)) {
+                                a_an = "an ";
+                        }
+                        System.out.println("Give me " + a_an  + letter + ": " + letter + "!");
+                        }
+
+                        // Repeat cheer
+                        while (i < times_to_cheer) {
+                        System.out.println(cheer + "!!!");
+                        i = i + 1;
+                        }
+                }
         }
-}
