7a5e9026aa635a39195de6a9702dbb72f11cda7f
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-06 10:38:17 +0200

    TimeCalc fix

--- TimeCalc.java
+++ TimeCalc.java
@@ -7,6 +7,23 @@ public class TimeCalc {
         int total_hour = total_minutes / 60;
         int new_hours = total_hour % 24;
         int new_minutes = total_minutes - (total_hour*60);
-        System.out.println(new_hours + ":" + new_minutes);
+        String str_minutes = "";
+        String str_hours = "";
+
+        		if (new_hours <10){
+			str_hours = "0"+ new_hours;
+		}
+		else{
+			str_hours =  String.valueOf(new_hours);
+        }
+        // Minutes Handling - if less then 10 minutes add "0"
+		if (new_minutes <10){
+			str_minutes = "0"+ new_minutes;
+		}
+		else{
+			str_minutes = String.valueOf(new_minutes);
+			
+		}
+        System.out.println(str_hours + ":" + str_minutes);
     }
 }
