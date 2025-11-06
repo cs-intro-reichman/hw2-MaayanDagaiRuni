5076b021e81a5537c0b0d250cc4fc8554a1db5ba
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-06 10:30:47 +0200

    TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,12 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutes_to_add = Integer.parseInt(args[1]);
+        int total_minutes = (hours * 60) + minutes + minutes_to_add;
+        int total_hour = total_minutes / 60;
+        int new_hours = total_hour % 24;
+        int new_minutes = total_minutes - (total_hour*60);
+        System.out.println(new_hours + ":" + new_minutes);
     }
 }
